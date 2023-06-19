41a9bed48014d9c7f8ce68a7e0cebc76f237a630
4GI-Corradi-Alessandro-2022-2023 <19364@studenti.marconiverona.edu.it>
2023-06-19 17:59:33 +0200

    creazione mapper e altre cose belle

--- chronos/src/main/java/it/addvalue/chronos/service/CarichiService.java
+++ chronos/src/main/java/it/addvalue/chronos/service/CarichiService.java
@@ -1,4 +1,35 @@
 package it.addvalue.chronos.service;
 
+import it.addvalue.chronos.core.exception.EsecuzioneErrataException;
+import it.addvalue.chronos.model.dto.carichiDTO;
+import it.addvalue.chronos.model.entity.CarichiEntity;
+import it.addvalue.chronos.repository.CarichiRepository;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Date;
+
+@NoArgsConstructor
 public class CarichiService {
+
+    CarichiRepository repoUno;
+
+    //metodo per eliminare elementi da una tabella
+
+    public boolean delete(ArrayList<carichiDTO> c) throws EsecuzioneErrataException {
+        for(carichiDTO car:c){
+            Month meseCarico=Month.of(car.getMese());
+            if(meseCarico.equals(Month.now())/*&& */){ // condizione da completare
+                //repoUno.delete(car);
+                System.out.println("sdfghjkl");
+            }else if(car==null){
+                throw new EsecuzioneErrataException("oggetto non trovato.");
+            }else{
+                throw new EsecuzioneErrataException("impossibile eseguire l'operazione.");
+            }
+        }
+        return true;
+    }
 }

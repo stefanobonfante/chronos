888d46635845a1cf857a868a598743d623ace02a
prettyboisosaa <midumfernando123@gmail.com>
2023-06-20 16:27:21 +0200

    sviluppi delete

--- chronos/src/main/java/it/addvalue/chronos/controller/SpeseController.java
+++ chronos/src/main/java/it/addvalue/chronos/controller/SpeseController.java
@@ -1,8 +1,5 @@
 package it.addvalue.chronos.controller;
 
-import java.io.IOException;
-import java.util.List;
-
 import it.addvalue.chronos.core.exception.Custom;
 import it.addvalue.chronos.model.dto.SpeseDto;
 import it.addvalue.chronos.model.entity.SpeseEntity;
@@ -11,41 +8,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/spese/")
 public class SpeseController {
 
-  private final SpeseService speservice;
 
-  @Autowired
-  public SpeseController(SpeseService speservice) {
-    this.speservice = speservice;
-  }
+    @Autowired
+    SpeseService service;
 
-  @GetMapping(path = "{anno}/{mese}/{giorno}/{codUtente}")
-  public ResponseEntity<List<SpeseEntity>> recuperoSpeseGiorno(
-      @PathVariable int anno,
-      @PathVariable int mese,
-      @PathVariable int giorno,
-      @PathVariable String codUtente)
-      throws IOException {
-    return ResponseEntity.ok(speservice.recuperoSpeseGiorno(anno, mese, giorno, codUtente));
-  }
+    @GetMapping(path = "{anno}/{mese}/{giorno}/{codUtente}")
+    public ResponseEntity<List<SpeseEntity>> recuperoSpeseGiorno(
+            @PathVariable int anno,
+            @PathVariable int mese,
+            @PathVariable int giorno,
+            @PathVariable String codUtente)
+            throws IOException {
+        return ResponseEntity.ok(service.recuperoSpeseGiorno(anno, mese, giorno, codUtente));
+    }
 
-  @GetMapping(path = "{anno}/{mese}/{codUtente}")
-  public ResponseEntity<List<SpeseEntity>> recuperoSpeseMese(
-      @PathVariable int anno, @PathVariable int mese, @PathVariable String codUtente)
-      throws IOException {
-    return ResponseEntity.ok(speservice.recuperoSpeseMese(anno, mese, codUtente));
-  }
+    @GetMapping(path = "{anno}/{mese}/{codUtente}")
+    public ResponseEntity<List<SpeseEntity>> recuperoSpeseMese(
+            @PathVariable int anno, @PathVariable int mese, @PathVariable String codUtente)
+            throws IOException {
+        return ResponseEntity.ok(service.recuperoSpeseMese(anno, mese, codUtente));
+    }
 
-  @PostMapping(path = "salvataggioSpese")
-  public void salvataggioSpese(@PathVariable List<SpeseDto> spese) throws IOException, Custom {
-    speservice.filtroSpese(spese);
-  }
+    @PostMapping(path = "salvataggioSpese")
+    public void salvataggioSpese(@PathVariable List<SpeseDto> spese) throws IOException, Custom {
+        service.filtroSpese(spese);
+    }
 
-  @PutMapping(path = "{List<SpeseDto>}")
-  public void modificaSpese(@PathVariable List<SpeseDto> spese) throws IOException, Custom {
-    speservice.filtroSpese(spese);
-  }
+    @PutMapping(path = "{List<SpeseDto>}")
+    public void modificaSpese(@PathVariable List<SpeseDto> spese) throws IOException, Custom {
+        service.filtroSpese(spese);
+    }
+
+    @DeleteMapping(path = "/api/spese/")
+    public void cancellaSpese(List<SpeseEntity> spese) {
+        service.deleatElementi(spese);
+
+    }
+
 }

7742af8fa301db10676a131cb20ade30839ba310
4GI-Corradi-Alessandro-2022-2023 <19364@studenti.marconiverona.edu.it>
2023-06-20 09:43:01 +0200

    creato Controller e creazione del metodo "delete"

--- chronos/src/main/java/it/addvalue/chronos/controller/CarichiController.java
+++ chronos/src/main/java/it/addvalue/chronos/controller/CarichiController.java
@@ -1,4 +1,27 @@
 package it.addvalue.chronos.controller;
 
+import it.addvalue.chronos.core.exception.EsecuzioneErrataException;
+import it.addvalue.chronos.model.dto.carichiDTO;
+import it.addvalue.chronos.service.CarichiService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class CarichiController {
+
+    private final CarichiService servizio;
+
+
+
+    //metodo per eliminare nel controller
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(List<carichiDTO> eliminazione) throws EsecuzioneErrataException {
+        return ResponseEntity.ok(servizio.delete(eliminazione));
+    }
 }

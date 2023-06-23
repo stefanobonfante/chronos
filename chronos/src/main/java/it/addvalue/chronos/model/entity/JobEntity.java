0db39aeffd2d435c5addaab7738c29d5d70a7bf2
Curello <cxrello0@gmail.com>
2023-06-23 14:35:31 +0200

    new files

---
+++ chronos/src/main/java/it/addvalue/chronos/model/entity/JobEntity.java
@@ -0,0 +1,20 @@
+package it.addvalue.chronos.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TB_JOBS")
+public class JobEntity {
+
+    
+
+}
---
+++ chronos/src/main/java/it/addvalue/chronos/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package it.addvalue.chronos.repository;
+
+import it.addvalue.chronos.model.entity.JobEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository {
+
+    @Query(value = "SELECT * FROM TB_JOBS WHERE COD_JOB=?1 AND COD_CLIENTE=?2")
+    List<JobEntity>
+}
